aa33a931d04e30d5750dfcd2708377c1ae10b92c
h0j02qp <hinkal.jain@walmart.com>
2024-01-29 12:37:26 +0530

    episode 2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# react-1
+# react-1  🚀
+
+## Parcel
+
+- Dev build
+- Local server
+- HMR - Hot module replacement
+- File watching algorithm - written in C++
+- Caching - faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
---
+++ app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "React !!"),
+    React.createElement("h2", {}, "H2 Tag !!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "H1 Tag !!"),
+    React.createElement("h2", {}, "H2 Tag !!"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
