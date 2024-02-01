342159af8ab95687314f1ddfd4689c1be8a0c5b4
h0j02qp <hinkal.jain@walmart.com>
2024-02-01 21:59:56 +0530

    episode-3

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "React !!"),
-    React.createElement("h2", {}, "H2 Tag !!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "H1 Tag !!"),
-    React.createElement("h2", {}, "H2 Tag !!"),
-  ]),
-]);
-
+const heading = React.createElement("h1", { id: "heading" }, "React 🚀");
+const jsxHeading = <h1 id="heading">React with JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//Component composition - component inside component
+const Title = () => {
+  return <h1 className="title">Title Compoenent</h1>;
+};
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">React Functional Compoenent</h1>
+  </div>
+);
+root.render(<HeadingComponent />);
+
+// element inside other element
+
+const element = <span>element</span>;
+
+const title = (
+  <div>
+    {element}
+    <h1>Title element</h1>
+  </div>
+);
+//root.render(title);
+
+// element inside component
+
+const element2 = <h1>React element</h1>;
+
+const ReactComponent = () => (
+  <div>
+    {element2}
+    <h1>React component</h1>
+  </div>
+);
+//root.render(<ReactComponent />);
+
+// React component inside react element
+
+const ReactComponent2 = () => <h1>React component</h1>;
+
+const reactElement = (
+  <div>
+    <ReactComponent2 />
+    <h2>React element</h2>
+  </div>
+);
+
+//root.render(reactElement);
